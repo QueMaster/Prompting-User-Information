c3d500d95c0453350a2e090ab598c2f169ec1246
Qhamaninande Rebe <67279129+QueMaster@users.noreply.github.com>
2021-07-20 22:17:41 +0200

    Add files via upload

---
+++ prompting user information.py
@@ -0,0 +1,25 @@
+#using pyhon simple variables.
+#Assigning values to variables.
+#Prompting users input
+
+append = "I am turning "
+
+#printing out the values entered 
+
+names = input ("What is your name & surname")
+print (names)
+print (" ")
+age = input ("What is his Age")
+print(append, age)
+print(" ")
+idNumber = input ("Give me your ID ")
+print(idNumber)
+print(" ")
+location = input ("What is your location")
+print(location)
+print(" ")
+occupation = input ("What are currently doing now")
+print(occupation)
+print(" ")
+cell = input ("Provide contact numbers")
+print(cell)
